8ae450598e3c1c52780386fea349474843ff6314
Acer76e <Acer76e@users.noreply.github.com>
2015-03-30 20:44:24 -0400

    Update views.py
    Created Orlando Layout

--- surgealert/views.py
+++ surgealert/views.py
@@ -6,30 +6,30 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-	santamonica = {'start_latitude':'34.019454','start_longitude':'-118.491191','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	manhattanbeach = {'start_latitude':'33.881728','start_longitude':'-118.404121','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	westhollywood = {'start_latitude':'34.085036','start_longitude':'-118.359318','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	koreatown = {'start_latitude':'34.069112','start_longitude':'-118.281384','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	midwilshire = {'start_latitude':'34.057166','start_longitude':'-118.323956','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	downtown = {'start_latitude':'34.042943','start_longitude':'-118.260098','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	glendale = {'start_latitude':'34.137907','start_longitude':'-118.250141','end_latitude':'34.019479','end_longitude':'-118.401718'}
-	northhollywood = {'start_latitude':'34.161161','start_longitude':'-118.358803','end_latitude':'34.019479','end_longitude':'-118.401718'}
+	downtowncore = {'start_latitude':'28.542613','start_longitude':'-81.378928','end_latitude':'28.542615','end_longitude':'-81.378930'}
+	outterdowntown = {'start_latitude':'28.554238','start_longitude':'-81.333994','end_latitude':'28.554240','end_longitude':'-81.333996'}
+	winterpark = {'start_latitude':'28.593242','start_longitude':'-81.350645','end_latitude':'28.593245','end_longitude':'-81.350648'}
+	fullsail = {'start_latitude':'28.596383','start_longitude':'-81.301328','end_latitude':'28.596385','end_longitude':'-81.301330'}
+	ucf = {'start_latitude':'28.597697','start_longitude':'-81.207055','end_latitude':'28.597699','end_longitude':'-81.207058'}
+	altamonte = {'start_latitude':'28.663386','start_longitude':'-81.365284','end_latitude':'28.663388','end_longitude':'-81.365286'}
+	curryford = {'start_latitude':'28.515967','start_longitude':'-81.290118','end_latitude':'28.515969','end_longitude':'-81.290120'}
+	wintersprings = {'start_latitude':'28.689733','start_longitude':'-81.209137','end_latitude':'28.689735','end_longitude':'-81.209139'}
 
 	cities = []
-	cities.append(santamonica)
-	cities.append(manhattanbeach)
-	cities.append(westhollywood)
-	cities.append(koreatown)
-	cities.append(midwilshire)
-	cities.append(downtown)
-	cities.append(glendale)
-	cities.append(northhollywood)
+	cities.append(downtowncore)
+	cities.append(outterdowntown)
+	cities.append(winterpark)
+	cities.append(fullsail)
+	cities.append(ucf)
+	cities.append(altamonte)
+	cities.append(curryford)
+	cities.append(wintersprings)
 	info = ''
-	names = ["Santa Monica","Manhattan Beach","West Hollywood","Koreatown","Midwilshire","Downtown","Glendale","North Hollywood"]
+	names = ["Orlando Downtown Core","Downtown Proper","Winter Park","Goldenrod Full Sail","UCF East Orlando","Altamonte Maitland","Curry Ford","Winter Springs"]
 
 	for i in cities:
 		payload = cities[cities.index(i)]
-		headers = {'Authorization':'Token ENTERYOURUBERTOKENHERE'}
+		headers = {'Authorization':'Token ydD_YSyQvmzZ8C5szSxeiZ-Qw4exupjar5otyuYv'}
 		r = requests.get('https://api.uber.com/v1/estimates/price', params = payload, headers=headers)
 		data = json.loads(r.text)
 		info = info + (names[cities.index(i)]) + '    '
